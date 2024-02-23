e27215ca53c52ec457d2d30714ffe5ee588ce386
Adisa-Shobi <s.oadisa.dev@gmail.com>
2024-02-23 18:07:30 +0000

    Update: states ans state by id endpoint implemented

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -1,31 +1,42 @@
 #!/usr/bin/python3
-"""
-Start web application with two routings
-"""
-
+'''
+Script runs flask web application listening on 0.0.0.0
+and port 5000
+'''
+from flask import Flask, render_template
 from models import storage
 from models.state import State
-from flask import Flask, render_template
 app = Flask(__name__)
 
 
-@app.route('/states')
-@app.route('/states/<id>')
-def states_list(id=None):
-    """Render template with states
-    """
-    path = '9-states.html'
+@app.route('/states', strict_slashes=False)
+def states_list():
+    '''
+    Default route for mimimal flask application
+    '''
     states = storage.all(State)
-    return render_template(path, states=states, id=id)
+    return render_template("9-states.html", states=states, state=None)
+
+
+@app.route('/states/<id>', strict_slashes=False)
+def state(id):
+    '''
+    Default route for mimimal flask application
+    '''
+    states = storage.all(State)
+    try:
+        queried_state = states["State.{}".format(id)]
+    except Exception:
+        queried_state = ""
+    return render_template("9-states.html", state=queried_state)
 
 
 @app.teardown_appcontext
-def app_teardown(arg=None):
-    """Clean-up session
-    """
+def teardown(exception):
+    '''Tears down the current sqlalchemy session after each request
+    '''
     storage.close()
 
 
-if __name__ == '__main__':
-    app.url_map.strict_slashes = False
-    app.run(host='0.0.0.0', port=5000)
+if __name__ == "__main__":
+    app.run(port=5000, host="0.0.0.0")
